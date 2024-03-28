055045a52f8714e1a475875d430359ba88667219
RandallKeur <108151926+RandallKeur7@users.noreply.github.com>
2024-03-28 11:09:20 -0400

    solve problem 713 and add tests

---
+++ src/main/java/com/leetcode/solutions/Problem713.java
@@ -0,0 +1,23 @@
+package com.leetcode.solutions;
+
+import java.util.ArrayList;
+
+public class Problem713 {
+    public int numSubarrayProductLessThanK(int[] nums, int max) {
+        if (max <= 1)
+            return 0;
+
+        int left = 0, right = 0, count = 0, product = 1;
+
+        while (right < nums.length) {
+            product *= nums[right];
+            while (product >= max) {
+                product /= nums[left++];
+            }
+            count += 1 + (right - left);
+            right++;
+        }
+
+        return count;
+    }
+}
