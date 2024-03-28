860b0cdae1f57897c2f46b2e8427a2baa922badf
RandallsBagels <59258405+RandallKeur@users.noreply.github.com>
2024-03-28 11:10:47 -0400

    solve problem 713 and add tests (#11)
    Co-authored-by: RandallKeur <108151926+RandallKeur7@users.noreply.github.com>

---
+++ src/test/java/com/leetcode/solutions/Problem713Test.java
@@ -0,0 +1,39 @@
+package com.leetcode.solutions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class Problem713Test {
+
+    private Problem713 serviceUnderTest;
+
+    private static Stream<Arguments> TestCases(){
+        return Stream.of(
+                Arguments.of(new int[] {10,5,2,6}, 100, 8),
+                Arguments.of(new int[] {1,2,3}, 0, 0 ),
+                Arguments.of(new int[] {1,2,3,4,5,6}, 60, 14),
+                Arguments.of(new int[] {1,5,2,6}, 70, 10),
+                Arguments.of(new int[] {2,2,2,2}, 9, 9)
+        );
+    }
+
+    @BeforeEach
+    public void setup() { this.serviceUnderTest = new Problem713(); }
+
+    @ParameterizedTest
+    @MethodSource("TestCases")
+    public void Problem713Test(int[] numbers, int max, int expected) {
+        // given & when
+        final var actual = serviceUnderTest.numSubarrayProductLessThanK(numbers, max);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+}
