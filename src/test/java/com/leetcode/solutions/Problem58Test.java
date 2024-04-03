7bfb9fc7f3e0c96b13e3b8ff1e64764c8cd2d5f0
RandallKeur <108151926+RandallKeur7@users.noreply.github.com>
2024-04-02 20:36:30 -0400

    solve problem 58 and add tests

---
+++ src/test/java/com/leetcode/solutions/Problem58Test.java
@@ -0,0 +1,37 @@
+package com.leetcode.solutions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class Problem58Test {
+
+    private Problem58 serviceUnderTest;
+
+    private static Stream<Arguments> TestCases() {
+        return Stream.of(
+                Arguments.of("Hello World", 5),
+                Arguments.of("   fly me   to   the moon  ", 4),
+                Arguments.of("luffy is still joyboy", 6),
+                Arguments.of("              ", 0)
+        );
+    }
+
+    @BeforeEach
+    public void setup() { this.serviceUnderTest = new Problem58(); }
+
+    @ParameterizedTest
+    @MethodSource("TestCases")
+    public void Problem58Tests(String input, int expected) {
+        // given & when
+        final var actual = serviceUnderTest.lengthOfLastWord(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+}
