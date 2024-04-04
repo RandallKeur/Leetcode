ea33b5ed410683ae07f652bb58064daf340fbcdd
RandallsBagels <59258405+RandallKeur@users.noreply.github.com>
2024-04-04 17:50:10 -0400

    solve problem 14 and add tests (#18)
    * solve problem 14 and add tests
    
    * re-format test and add another
    
    ---------
    
    Co-authored-by: RandallKeur <108151926+RandallKeur7@users.noreply.github.com>

---
+++ src/main/java/com/leetcode/solutions/problems/Problem14.java
@@ -0,0 +1,13 @@
+package com.leetcode.solutions.problems;
+
+public class Problem14 {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (String s : strs)
+            while (s.indexOf(prefix) != 0)
+                prefix = prefix.substring(0, prefix.length() - 1);
+        return prefix;
+    }
+}
---
+++ src/test/java/com/leetcode/solutions/problems/Problem14Test.java
@@ -0,0 +1,37 @@
+package com.leetcode.solutions.problems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class Problem14Test {
+
+    private Problem14 serviceUnderTest;
+
+    private static Stream<Arguments> TestCases() {
+        return Stream.of(
+                Arguments.of(new String[] {"flower","flow","flight"}, "fl"),
+                Arguments.of(new String[] {"dog","racecar","car"}, ""),
+                Arguments.of(new String[] {"test","tes","testing", "tested"}, "tes")
+        );
+    }
+
+    @BeforeEach
+    public void setup() { this.serviceUnderTest = new Problem14(); }
+
+    @ParameterizedTest
+    @MethodSource("TestCases")
+    public void Problem14Test(String[] input, String expected) {
+        // given && when
+        final var actual = serviceUnderTest.longestCommonPrefix(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+}
