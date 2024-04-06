ec873c1642db58d51a8e86997aebd8722701d847
RandallKeur <108151926+RandallKeur7@users.noreply.github.com>
2024-04-06 08:11:52 -0400

    solve problem 1544 and add tests

---
+++ src/test/java/com/leetcode/solutions/problems/Problem1544Test.java
@@ -0,0 +1,36 @@
+package com.leetcode.solutions.problems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class Problem1544Test {
+
+    private Problem1544 serviceUnderTest;
+
+    private static Stream<Arguments> TestCases() {
+        return Stream.of(
+                Arguments.of("leEeetcode", "leetcode"),
+                Arguments.of("abBAcC", ""),
+                Arguments.of("s", "s")
+        );
+    }
+
+    @BeforeEach
+    public void setup() { this.serviceUnderTest = new Problem1544(); }
+
+    @ParameterizedTest
+    @MethodSource("TestCases")
+    public void Problem1544Test(String input, String expected) {
+        // given && when
+        final var actual = serviceUnderTest.makeGood(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+}
