093319f4588a6d8e12f419593716bd9a12332cc5
RandallKeur <108151926+RandallKeur7@users.noreply.github.com>
2024-04-06 08:27:03 -0400

    solve problem 1249 and add tests

---
+++ src/test/java/com/leetcode/solutions/problems/Problem1249Test.java
@@ -0,0 +1,36 @@
+package com.leetcode.solutions.problems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class Problem1249Test {
+
+    private Problem1249 serviceUnderTest;
+
+    private static Stream<Arguments> TestCases() {
+        return Stream.of(
+                Arguments.of("lee(t(c)o)de", "lee(t(c)o)de"),
+                Arguments.of("a)b(c)d", "ab(c)d"),
+                Arguments.of("))((", "")
+        );
+    }
+
+    @BeforeEach
+    public void setup() { this.serviceUnderTest = new Problem1249(); }
+
+    @ParameterizedTest
+    @MethodSource("TestCases")
+    public void Problem1249Test(String input, String expected) {
+        // when && then
+        final var actual = serviceUnderTest.minRemoveToMakeValid(input);
+
+        //then
+        then(actual).isEqualTo(expected);
+    }
+}
