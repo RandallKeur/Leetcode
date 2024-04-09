1f981de7e9bd401df264c0e63f3548e9e74383c4
RandallsBagels <59258405+RandallKeur@users.noreply.github.com>
2024-04-09 15:17:24 -0400

    solve problem 2073 and add tests (#21)
    Co-authored-by: RandallKeur <108151926+RandallKeur7@users.noreply.github.com>

---
+++ src/test/java/com/leetcode/solutions/problems/Problem2073Test.java
@@ -0,0 +1,37 @@
+package com.leetcode.solutions.problems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class Problem2073Test {
+
+    private Problem2073 serviceUnderTest;
+
+
+    private static Stream<Arguments> TestCases() {
+        return Stream.of(
+                Arguments.of(new int[] {2,3,2}, 2, 6),
+                Arguments.of(new int[] {5,1,1,1}, 0, 8),
+                Arguments.of( new int[] {5,1,1,1}, 1, 2),
+                Arguments.of( new int[] {5,1,2,2,1}, 3, 8)
+        );
+    }
+    @BeforeEach
+    public void setup() { this.serviceUnderTest = new Problem2073(); }
+
+    @ParameterizedTest
+    @MethodSource("TestCases")
+    public void Problem2073Test(int[] tickets, int position, int expected) {
+        // given && when
+        final var actual = serviceUnderTest.timeRequiredToBuy(tickets, position);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+}
