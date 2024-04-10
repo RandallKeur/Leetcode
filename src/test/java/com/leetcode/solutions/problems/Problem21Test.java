52acb499769a8b4910ab726b5583a6d2af828724
RandallsBagels <59258405+RandallKeur@users.noreply.github.com>
2024-04-10 12:03:21 -0400

    solve problem 21 and add tests (#24)
    Co-authored-by: RandallKeur <108151926+RandallKeur7@users.noreply.github.com>

---
+++ src/test/java/com/leetcode/solutions/problems/Problem21Test.java
@@ -0,0 +1,48 @@
+package com.leetcode.solutions.problems;
+
+import com.leetcode.solutions.customStructures.ListNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class Problem21Test {
+
+    private Problem21 serviceUnderTest;
+
+    private static Stream<Arguments> TestCases() {
+        return Stream.of(
+                Arguments.of(null, new ListNode(0, null), new ListNode(0, null)),
+                Arguments.of(new ListNode(1, new ListNode(2, new ListNode(4, null))),
+                        new ListNode(1, new ListNode(3, new ListNode(4))),
+                new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3,
+                new ListNode(4, new ListNode(4, null))))))),
+                Arguments.of(new ListNode(1, new ListNode(4, null)),
+                        new ListNode(2, new ListNode(2, new ListNode(3, null))),
+                        new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(3,
+                                new ListNode(4, null))))))
+        );
+    }
+
+    @BeforeEach
+    public void setup() { this .serviceUnderTest = new Problem21(); }
+
+    @ParameterizedTest
+    @MethodSource("TestCases")
+    public void Problem21Test(ListNode list1, ListNode list2, ListNode expected) {
+        // given && when
+        var actual = serviceUnderTest.mergeTwoLists(list1, list2);
+
+        // then
+        then(actual).isInstanceOf(expected.getClass());
+        while (expected.next != null) {
+            then(expected.val).isEqualTo(actual.val);
+            expected = expected.next;
+            actual = actual.next;
+        }
+    }
+}
