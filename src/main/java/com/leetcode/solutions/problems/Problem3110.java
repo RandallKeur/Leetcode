6d453657d2af60564c6c3d1f0e94f40fc41d432b
RandallKeur <rkeur7@gmail.com>
2024-07-13 07:18:50 -0400

    clean up 2 problems for readability

--- src/main/java/com/leetcode/solutions/problems/Problem3110.java
+++ src/main/java/com/leetcode/solutions/problems/Problem3110.java
@@ -7,8 +7,12 @@ public class Problem3110 {
     public int scoreOfString(String input) {
         var score = 0;
         for (var i = 0; i < input.length() - 1; i++) {
-            score += abs((int) input.charAt(i) - (int) input.charAt(i+1));
+            score += Math.abs(getAsciiValue(input.charAt(i)) - getAsciiValue(input.charAt(i+1)));
         }
         return score;
+    }
+
+    private int getAsciiValue(char character) {
+        return character;
     }
 }
