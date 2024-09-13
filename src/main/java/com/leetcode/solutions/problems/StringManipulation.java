b1a6f72615572ad7741c3881e3ec9a7fd183ad26
Randall Keur <rkeur7@gmail.com>
2024-09-13 10:19:31 -0400

    refactor all string manipulation related classes, tests, etc.

---
+++ src/main/java/com/leetcode/solutions/problems/StringManipulation.java
@@ -0,0 +1,345 @@
+package com.leetcode.solutions.problems;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class StringManipulation {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+
+    public String convertToZigZag(String input, int numberOfRows) {
+        if (numberOfRows == 1)
+            return input;
+
+        StringBuilder result = new StringBuilder();
+        int length = input.length();
+        int columnDiff = 2 * (numberOfRows - 1);
+        int diagonalDiff, diagonalIndex, columnIndex;
+        for (int i = 0; i < numberOfRows; i++) {
+            columnIndex = i;
+
+            while (columnIndex < length) {
+                result.append(input.charAt(columnIndex));
+                if (i != 0 && i != numberOfRows - 1) {
+                    diagonalDiff = columnDiff - 2 * i;
+                    diagonalIndex = columnIndex + diagonalDiff;
+
+                    if (diagonalIndex < length) {
+                        result.append(input.charAt(diagonalIndex));
+                    }
+                }
+                columnIndex += columnDiff;
+            }
+        }
+
+        return result.toString();
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (String s : strs)
+            while (s.indexOf(prefix) != 0)
+                prefix = prefix.substring(0, prefix.length() - 1);
+        return prefix;
+    }
+
+    public Boolean isValid(String input) {
+        var stack = new Stack<Character>();
+        for (var character : input.toCharArray()) {
+            if (character == '(')
+                stack.push(')');
+            else if (character == '[')
+                stack.push(']');
+            else if (character == '{')
+                stack.push('}');
+            else if (stack.empty() || stack.pop() != character)
+                return Boolean.FALSE;
+        }
+        return stack.isEmpty();
+    }
+
+    public int lengthOfLastWord(String input) {
+
+        final var array = input.split(" ");
+        var lastWord = "";
+
+        for (var text : array) {
+            lastWord = text;
+        }
+
+        return lastWord.length();
+    }
+
+    public boolean isIsomorphic(String input1, String input2) {
+
+        var map = new HashMap<Character, Character>();
+        var length = input1.length();
+
+        for (var i = 0; i < length; i++) {
+            var record = map.get(input1.charAt(i));
+            var valueExists = map.containsValue(input2.charAt(i));
+            if (record == null) {
+                if (valueExists) return Boolean.FALSE;
+                map.put(input1.charAt(i), input2.charAt(i));
+            } else {
+                if (!record.equals(input2.charAt(i)))
+                    return Boolean.FALSE;
+            }
+        }
+
+        return Boolean.TRUE;
+    }
+
+    public boolean isAnagram(String firstWord, String secondWord) {
+        var dictionary = new HashMap<Character, Integer>();
+        countUpLetters(dictionary, firstWord.toLowerCase());
+        countDownLetters(dictionary, secondWord.toLowerCase());
+        return determineAnagram(dictionary);
+    }
+
+
+    private boolean determineAnagram(HashMap<Character, Integer> dictionary) {
+        for (Integer value : dictionary.values()) {
+            if (value != 0) {
+                return Boolean.FALSE;
+            }
+        }
+        return Boolean.TRUE;
+    }
+
+    private void countUpLetters(HashMap<Character, Integer> dictionary, String firstWord) {
+        for (char letter : firstWord.toCharArray()) {
+            var currentCount = dictionary.getOrDefault(letter, 0);
+            dictionary.put(letter, currentCount + 1);
+        }
+    }
+
+    private void countDownLetters(HashMap<Character, Integer> dictionary, String secondWord) {
+        for (char letter : secondWord.toCharArray()) {
+            var currentCount = dictionary.getOrDefault(letter, 0);
+            dictionary.put(letter, currentCount - 1);
+        }
+    }
+
+    public Boolean isSubsequence(String subsequence, String stringToCheck) {
+        var subsequenceCharArray = subsequence.toCharArray();
+        var startingIndex = 0;
+        var newIndex = 0;
+        for (char c : subsequenceCharArray) {
+            if (stringToCheck.length() < startingIndex + 1) {
+                return false;
+            }
+            newIndex = stringToCheck.indexOf(c, startingIndex);
+            if (newIndex == -1) {
+                return false;
+            }
+            startingIndex = newIndex + 1;
+        }
+        return true;
+    }
+
+    public Boolean isSubsequenceV2(String subsequence, String stringToCheck) {
+        var subsequenceCharArray = subsequence.toCharArray();
+        for (char c : subsequenceCharArray) {
+            var indexOfCharacter = stringToCheck.indexOf(c);
+            if (indexOfCharacter == -1) {
+                return false;
+            }
+            stringToCheck = stringToCheck.substring(indexOfCharacter + 1);
+        }
+        return true;
+    }
+
+    public boolean repeatedSubstringPattern(String s) {
+        final var charArray = s.toCharArray();
+        var workingString = new StringBuilder();
+        var repeats = 0;
+        var stringToTest = "";
+        final var stringLength = s.length();
+        if (s.length() == 1) {
+            return Boolean.FALSE;
+        }
+        for (char c : charArray) {
+            workingString.append(c);
+
+            if (stringLength % workingString.length() != 0)
+                continue;
+
+            if (workingString.length() > stringLength / 2)
+                return false;
+
+            repeats = stringLength / workingString.length();
+            stringToTest = workingString.toString().repeat(repeats);
+            if (stringToTest.equals(s))
+                return true;
+        }
+        return false;
+    }
+
+    public Boolean checkValidString(String input) {
+        int min = 0, max = 0;
+        for (var letter : input.toCharArray()) {
+            if (letter == '(') {
+                min++;
+                max++;
+            } else if (letter == ')') {
+                min--;
+                max--;
+            } else if (letter == '*') {
+                min--;
+                max++;
+            }
+            if (max < 0)
+                return false;
+
+            min = Math.max(min, 0);
+        }
+        return min == 0;
+    }
+
+    public String removeDuplicates(String s) {
+        var resultStack = new Stack<Character>();
+        var sCharArray = s.toCharArray();
+        for (char c : sCharArray) {
+            if (resultStack.empty()
+            ) {
+                resultStack.push(c);
+            } else if (resultStack.peek() == c) {
+                resultStack.pop();
+            } else {
+                resultStack.push(c);
+            }
+        }
+        return convertStackToString(resultStack);
+    }
+
+    private String convertStackToString(Stack<Character> input) {
+        return input.stream().map(Object::toString).collect(Collectors.joining(""));
+    }
+
+    public String removeDuplicatesRecursion(String s) {
+
+        var inputAsCharArray = s.toCharArray();
+        var i = 0;
+
+        while (i < inputAsCharArray.length - 1) {
+            if (inputAsCharArray[i] == inputAsCharArray[i + 1]) {
+                var newString = buildNewString(i, s);
+                return removeDuplicatesRecursion(newString);
+            } else i++;
+        }
+
+        return s;
+    }
+
+    private String buildNewString(Integer index, String s) {
+        if (index == 0) {
+            return s.substring(2);
+        }
+        if (index + 2 > s.length()) {
+            return s.substring(0, index);
+        }
+        var left = s.substring(0, index);
+        var right = s.substring(index + 2);
+        return left.concat(right);
+    }
+
+    public String minRemoveToMakeValid(String input) {
+        final var stack = new Stack<Integer>();
+        for (var i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(')
+                stack.push(i);
+            else if (input.charAt(i) == ')') {
+                if (!stack.isEmpty() && input.charAt(stack.peek()) == '(') {
+                    stack.pop();
+                } else stack.push(i);
+            }
+        }
+        return buildResult(stack, input);
+    }
+
+    private String buildResult(Stack<Integer> stack, String input) {
+        final var result = new StringBuilder();
+        for (var i = input.length() - 1; i >= 0; i--) {
+            if (!stack.isEmpty() && stack.peek() == i) {
+                stack.pop();
+            } else result.append(input.charAt(i));
+        }
+        return result.reverse().toString();
+    }
+
+    public String makeGood(String s) {
+
+        final var CHAR_CONSTANT = 32;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char character : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(character - stack.peek()) == CHAR_CONSTANT) {
+                stack.pop();
+            } else {
+                stack.push(character);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+        }
+
+        return result.toString();
+    }
+
+    public int minimizedStringLength(String s) {
+        return buildUniqueCharacterSet(s).size();
+    }
+
+    private HashSet<Character> buildUniqueCharacterSet(String s) {
+        var inputAsCharArray = s.toCharArray();
+        var uniqueChars = new HashSet<Character>();
+        for (char c : inputAsCharArray) {
+            uniqueChars.add(c);
+        }
+        return uniqueChars;
+    }
+
+    public int minimizedStringLengthWithoutAdjacentRepeatingCharacters(String s) {
+        var inputAsCharArray = s.toCharArray();
+        var returnList = buildUniqueAdjacentCharacterList(inputAsCharArray);
+        return returnList.size();
+    }
+
+    private ArrayList<Character> buildUniqueAdjacentCharacterList(char[] input) {
+        var resultList = new ArrayList<Character>();
+        resultList.add(input[0]);
+        var i = 1;
+        var previousCharacter = input[0];
+
+        while (i < input.length) {
+            if (input[i] != previousCharacter) {
+                resultList.add(input[i]);
+                previousCharacter = input[i];
+            }
+            i++;
+        }
+
+        return resultList;
+    }
+
+    public int scoreOfString(String input) {
+        var score = 0;
+        for (var i = 0; i < input.length() - 1; i++) {
+            score += Math.abs(getAsciiValue(input.charAt(i)) - getAsciiValue(input.charAt(i + 1)));
+        }
+        return score;
+    }
+
+    private int getAsciiValue(char character) {
+        return character;
+    }
+}
---
+++ src/test/java/com/leetcode/solutions/problems/StringManipulationTest.java
@@ -0,0 +1,345 @@
+package com.leetcode.solutions.problems;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class StringManipulationTest {
+
+
+    private StringManipulation serviceUnderTest;
+
+    private static Stream<Arguments> DefangIpAddressTestCases() {
+        return Stream.of(
+                Arguments.of("1.1.1.1", "1[.]1[.]1[.]1"),
+                Arguments.of("255.100.50.0", "255[.]100[.]50[.]0")
+        );
+    }
+
+    private static Stream<Arguments> ConvertZigZagTestCases() {
+        return Stream.of(
+                Arguments.of("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"),
+                Arguments.of("PAYPALISHIRING", 4, "PINALSIGYAHRPI"),
+                Arguments.of("A", 1, "A"),
+                Arguments.of("PAYPALISHIRING", 6, "PRAIIYHNPSGAIL"),
+                Arguments.of("PAYPALISHIRINGDEVELOPERS", 8, "PDAGEYNVPIEARLLIOSIHPRSE")
+        );
+    }
+
+    private static Stream<Arguments> LongestCommonPrefixTestCases() {
+        return Stream.of(
+                Arguments.of(new String[]{"flower", "flow", "flight"}, "fl"),
+                Arguments.of(new String[]{"dog", "racecar", "car"}, ""),
+                Arguments.of(new String[]{"test", "tes", "testing", "tested"}, "tes")
+        );
+    }
+
+    private static Stream<Arguments> IsValidParenthesisTestCases() {
+        return Stream.of(
+                Arguments.of("{}[]()", Boolean.TRUE),
+                Arguments.of("{]", Boolean.FALSE),
+                Arguments.of("{[()]]", Boolean.FALSE),
+                Arguments.of("[", Boolean.FALSE)
+        );
+    }
+
+    private static Stream<Arguments> LengthOfLastWordTestCases() {
+        return Stream.of(
+                Arguments.of("Hello World", 5),
+                Arguments.of("   fly me   to   the moon  ", 4),
+                Arguments.of("luffy is still joyboy", 6),
+                Arguments.of("              ", 0)
+        );
+    }
+
+    private static Stream<Arguments> IsIsomorphicTestCases() {
+        return Stream.of(
+                Arguments.of("egg", "add", true),
+                Arguments.of("foo", "bar", false),
+                Arguments.of("food", "eggs", true),
+                Arguments.of("food", "acai", false),
+                Arguments.of("paper", "title", true),
+                Arguments.of("qwerasdfvcxz", "zxcvasdfqwer", true),
+                Arguments.of("badc", "baba", false),
+                Arguments.of("baba", "badc", false)
+        );
+    }
+
+    private static Stream<Arguments> IsAnagramTestCases() {
+        return Stream.of(
+                Arguments.of("anagram", "nagaram", Boolean.TRUE),
+                Arguments.of("rat", "car", Boolean.FALSE),
+                Arguments.of("Listen", "Silent", Boolean.TRUE),
+                Arguments.of("Save", "Vase", Boolean.TRUE),
+                Arguments.of("savvy", "Save", Boolean.FALSE)
+        );
+    }
+
+    private static Stream<Arguments> IsSubsequenceTestCases() {
+        return Stream.of(
+                Arguments.of("abc", "ahbgdc", true),
+                Arguments.of("axc", "ahbgdc", false),
+                Arguments.of("abbd", "adksjfdlkbdlsfkjdfbfe", false),
+                Arguments.of("abbd", "asdfkjsdlfkbfsbfd", true),
+                Arguments.of("abbbd", "abbbbbd", true),
+                Arguments.of("abbbd", "abbd", false),
+                Arguments.of("abbc", "aerebereb", false)
+        );
+    }
+
+    private static Stream<Arguments> RepeatedSubstringPatternTestCases() {
+        return Stream.of(
+                Arguments.of("abab", true),
+                Arguments.of("aba", false),
+                Arguments.of("abcabcabcabc", true),
+                Arguments.of("aa", true),
+                Arguments.of("a", false)
+        );
+    }
+
+    private static Stream<Arguments> CheckValidStringTestCases() {
+        return Stream.of(
+                Arguments.of("()", true),
+                Arguments.of("(*)", true),
+                Arguments.of("(*))", true),
+                Arguments.of("(***()))", true)
+        );
+    }
+
+    private static Stream<Arguments> RemoveDuplicatesTestCases() {
+        return Stream.of(
+                Arguments.of("abbaca", "ca"),
+                Arguments.of("azxxzy", "ay"),
+                Arguments.of("bbacbbddc", "a"),
+                Arguments.of("baabcbbaabbc", ""),
+                Arguments.of("abbacabcddcba", "c"),
+                Arguments.of("abb", "a"),
+                Arguments.of("bba", "a")
+        );
+    }
+
+    private static Stream<Arguments> MinRemoveToMakeValidTestCases() {
+        return Stream.of(
+                Arguments.of("lee(t(c)o)de", "lee(t(c)o)de"),
+                Arguments.of("a)b(c)d", "ab(c)d"),
+                Arguments.of("))((", "")
+        );
+    }
+
+    private static Stream<Arguments> MakeGoodTestCases() {
+        return Stream.of(
+                Arguments.of("leEeetcode", "leetcode"),
+                Arguments.of("abBAcC", ""),
+                Arguments.of("s", "s")
+        );
+    }
+
+    private static Stream<Arguments> MinimizedStringLengthTestCases() {
+        return Stream.of(
+                Arguments.of("aaabc", 3),
+                Arguments.of("cbbd", 3),
+                Arguments.of("dddaaa", 2)
+        );
+    }
+
+    private static Stream<Arguments> WithoutAdjacentTestCases() {
+        return Stream.of(
+                Arguments.of("aaaabcd", 4),
+                Arguments.of("aaabacd", 5),
+                Arguments.of("aaab", 2),
+                Arguments.of("aabbaabac", 6),
+                Arguments.of("cbbdb", 4)
+        );
+    }
+
+    private static Stream<Arguments> ScoreOfStringTestCases() {
+        return Stream.of(
+                Arguments.of("hello", 13),
+                Arguments.of("zaz", 50),
+                Arguments.of("hh", 0),
+                Arguments.of("rememberthetitans", 155)
+        );
+    }
+
+    @BeforeEach
+    public void setup() {
+        this.serviceUnderTest = new StringManipulation();
+    }
+
+    @ParameterizedTest
+    @MethodSource("DefangIpAddressTestCases")
+    public void testDefangIpAddress_withParameterizedInputs(String input, String expected) {
+
+        //when
+        final var actual = serviceUnderTest.defangIPaddr(input);
+
+        //then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("ConvertZigZagTestCases")
+    public void testConvertZigZag_withParameterizedInputs(String input, int numberOfRows, String expected) {
+        // given & when
+        final var actual = serviceUnderTest.convertToZigZag(input, numberOfRows);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("LongestCommonPrefixTestCases")
+    public void testLongestCommonPrefix_withParameterizedInputs(String[] input, String expected) {
+        // given && when
+        final var actual = serviceUnderTest.longestCommonPrefix(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("IsValidParenthesisTestCases")
+    public void testIsValidParenthesis_withParameterizedInputs(String input, Boolean expected) {
+        // given && when
+        final var actual = serviceUnderTest.isValid(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("LengthOfLastWordTestCases")
+    public void testLengthOfLastWord_withParameterizedInputs(String input, int expected) {
+        // given & when
+        final var actual = serviceUnderTest.lengthOfLastWord(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("IsIsomorphicTestCases")
+    public void testIsomorphic_withParameterizedInputs(String input1, String input2, boolean expected) {
+        // given and when
+        final var actual = serviceUnderTest.isIsomorphic(input1, input2);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("IsAnagramTestCases")
+    public void testAnagram_withParameterizedInputs(String firstWord, String secondWord, boolean expected) {
+        // given & when
+        final var actual = serviceUnderTest.isAnagram(firstWord, secondWord);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("IsSubsequenceTestCases")
+    public void testIsSubsequence_withParameterizedInputs(String string1, String string2, Boolean expected) {
+        // given and when
+        final var actual = serviceUnderTest.isSubsequence(string1, string2);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("IsSubsequenceTestCases")
+    public void testIsSubsequenceV2_withParameterizedInputs(String string1, String string2, Boolean expected) {
+        // given and when
+        final var actual = serviceUnderTest.isSubsequenceV2(string1, string2);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("RepeatedSubstringPatternTestCases")
+    public void testRepeatedSubstringPattern_withParameterizedInputs(String input, boolean expected) {
+        // given & when
+        final var actual = serviceUnderTest.repeatedSubstringPattern(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("CheckValidStringTestCases")
+    public void testCheckValidString_withParameterizedInputs(String input, Boolean expected) {
+        // given && when
+        final var actual = serviceUnderTest.checkValidString(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("RemoveDuplicatesTestCases")
+    public void testRemoveDuplicates_withParameterizedInputs(String input, String expected) {
+
+        //when
+        final var actual = serviceUnderTest.removeDuplicates(input);
+
+        //then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("MinRemoveToMakeValidTestCases")
+    public void testMinRemoveToMakeValid_withParameterizedInputs(String input, String expected) {
+        // when && then
+        final var actual = serviceUnderTest.minRemoveToMakeValid(input);
+
+        //then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("MakeGoodTestCases")
+    public void testMakeGood_withParameterizedInputs(String input, String expected) {
+        // given && when
+        final var actual = serviceUnderTest.makeGood(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("MinimizedStringLengthTestCases")
+    public void testSolution_withParameterizedInputs(String input, int expected) {
+        //when
+        final var actual = serviceUnderTest.minimizedStringLength(input);
+
+        //then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("WithoutAdjacentTestCases")
+    public void testWithoutAdjacentSolution_withParameterizedInputs(String input, int expected) {
+        //when
+        final var actual = serviceUnderTest.minimizedStringLengthWithoutAdjacentRepeatingCharacters(input);
+
+        //then
+        then(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("ScoreOfStringTestCases")
+    public void testScoreOfString_withParameterizedTest(String input, Integer expected) {
+        // given && when
+        final var actual = serviceUnderTest.scoreOfString(input);
+
+        // then
+        then(actual).isEqualTo(expected);
+    }
+}
