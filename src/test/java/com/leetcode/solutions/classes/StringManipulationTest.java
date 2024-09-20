81b2ef70adb2a93701e4b00d3e6754b4abbf4d4f
Randall Keur <rkeur7@gmail.com>
2024-09-20 10:52:00 -0400

    re-format

--- src/test/java/com/leetcode/solutions/classes/StringManipulationTest.java
+++ src/test/java/com/leetcode/solutions/classes/StringManipulationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -170,9 +169,9 @@ public class StringManipulationTest {
 
     public static Stream<Arguments> FindWordsContainingTestCases() {
         return Stream.of(
-                Arguments.of(new String[] {"leet", "code" }, "e", List.of(0, 1)),
-                Arguments.of(new String[] { "abc","bcd","aaaa","cbc" }, "a", List.of(0,2)),
-                Arguments.of(new String[] {"abc","bcd","aaaa","cbc"}, "z", List.of())
+                Arguments.of(new String[]{"leet", "code"}, "e", List.of(0, 1)),
+                Arguments.of(new String[]{"abc", "bcd", "aaaa", "cbc"}, "a", List.of(0, 2)),
+                Arguments.of(new String[]{"abc", "bcd", "aaaa", "cbc"}, "z", List.of())
         );
     }
 
